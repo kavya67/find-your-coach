fce5b4bafcd353999d287e8d7d252ea7e9b9e494
Kavya <kavyamaniyan13@gmail.com>
2019-07-06 20:04:41 +0530

    modified components file

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default App
---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import {Provider} from 'react-redux'
+
+import App from './App'
+
+const jsx = <Provider>
+                <App/>
+            </Provider>
+
+ReactDOM.render(jsx, document.getElementById('root'))
+
