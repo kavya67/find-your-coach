4e772d1851b8dd5d9e598222fd843146a8c45c18
web-geek <PremK.Lf@gmail.com>
2019-07-11 19:57:56 +0530

    added coach model and controller

--- app/controllers/coachController.js
+++ app/controllers/coachController.js
@@ -1,22 +1,10 @@
-const express = require("express");
-const router = express.Router();
+const router = require("express").Router();
 
 const {authenticateUser} = require("../middleware/authenticateUser");
 const {authorizeUser} = require("../middleware/authorizeUser");
 const Coach = require("../models/coach");
 
-router.get("/", (req, res) => {
-    Coach.find().populate("userId")
-        .then(coaches => { res.send(coaches) })
-        .catch(err => { res.status(500).send({ status: "internal server issue" }) })
-})
-
-router.get("/:id", (req, res) => {
-    const id = req.params.id
-    Coach.findById(id).populate("category")
-        .then(coaches => { res.send(coaches) })
-        .catch(err => { res.status(500).send({ status: "internal server issue" }) })
-})
+//localhost:3004/coach/
 
 router.post("/", authenticateUser, authorizeUser, (req, res) => {
     const body = req.body
@@ -26,17 +14,34 @@ router.post("/", authenticateUser, authorizeUser, (req, res) => {
         .catch(err => res.send(err))
 })
 
+//localhost:3004/coach/
+
+router.get("/", (req, res) => {
+    Coach.find().populate("userId")
+        .then(coaches => { res.send(coaches) })
+        .catch(err => { res.status(500).send({ status: "internal server issue" }) })
+})
+
+router.get("/:id", (req, res) => {
+    const id = req.params.id
+    Coach.findById(id).populate("user")
+        .then(coaches => { res.send(coaches) })
+        .catch(err => { res.status(500).send({ status: "internal server issue" }) })
+})
+
 router.put("/:id", authenticateUser, authorizeUser, (req, res) => {
     const id = req.params.id
+    const user = req.user
     const body = req.body
-    Coach.findByIdAndUpdate({_id: id}, {$set: body}, {new: true, runValidators: true})
+    Coach.findByOneAndUpdate({_id: id, user: user._id}, {$set: body}, {new: true, runValidators: true})
         .then(coach => res.send(coach))
         .catch(err => res.send(err))
 })
 
 router.delete("/:id", authenticateUser, authorizeUser, (req, res) => {
     const id = req.params.id
-    Coach.findByIdAndDelete(id)
+    const user = req.user
+    Coach.findOneAndDelete({_id: id, user: user._id})
         .then(coach => res.send(coach))
         .catch(err => res.status(500).send({ status: "internal server issue" }))
 })
--- app/models/coach.js
+++ app/models/coach.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const coachSchema = new Schema({
-    imageUrl = {
+    imageUrl: {
         type: String
     },
-    description = {
+    description: {
         type: String
     },
-    locations = [
+    locations: [
         {
             location: {
                 type: Schema.Types.ObjectId,
@@ -16,7 +16,7 @@ const coachSchema = new Schema({
             }
         }
     ],
-    userId = {
+    user: {
         type: Schema.Types.ObjectId,
         ref: "User"
     },
