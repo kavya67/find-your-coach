bb48fc309765ec955ffbac0611f2713aa3eba206
web-geek <PremK.Lf@gmail.com>
2019-07-13 21:50:58 +0530

    booking model and controllers added

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const userRouter = require('./app/controllers/userController');
 const coachRouter = require("./app/controllers/coachController");
 const locationRouter = require("./app/controllers/locationController");
 const reviewRouter = require("./app/controllers/reviewController");
+const bookingRouter = require("./app/controllers/bookingController");
 
 const port = 3004;
 
@@ -17,6 +18,7 @@ app.use('/users', userRouter);
 app.use('/coach', coachRouter);
 app.use('/locations', locationRouter);
 app.use('/reviews', reviewRouter);
+app.use('/booking', bookingRouter);
 
 app.listen(port,()=>{
     console.log('listening to port', port);
